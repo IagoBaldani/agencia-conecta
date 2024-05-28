511345bcadd7d54399ef5a8f08dfd92f43f3f34d
Iago Baldani <iagobaldanialmeida@hotmail.com>
2024-05-28 11:23:36 -0300

    FEAT - ESTRUTURAL - Conclusao tela calculadora e influenciadores.

--- src/app/pages/influenciadores/influenciadores.component.ts
+++ src/app/pages/influenciadores/influenciadores.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MenuComponent} from "../../components/menu/menu.component";
+import {NgxMaskDirective} from "ngx-mask";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-influenciadores',
   standalone: true,
-  imports: [MenuComponent],
+    imports: [MenuComponent, NgxMaskDirective, ReactiveFormsModule],
   templateUrl: './influenciadores.component.html',
   styleUrl: './influenciadores.component.scss'
 })
